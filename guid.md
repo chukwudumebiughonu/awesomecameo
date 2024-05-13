c83d7ea3f19a015c525dc4a1b154d0966f13d0e8
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-05-13 22:46:28 +0100

    first commit

---
+++ guid.md
@@ -0,0 +1,11 @@
+create and initialize app with : pnpm dlx create-next-app@latest
+
+Initialize ui components with: npx shadcn-ui@latest init
+
+Install lucide icon with: pnpm install lucide-react 
+
+Add component from shadcn-ui with:npx shadcn-ui@latest add button
+
+Install kinde with: pnpm install @kinde-oss/kinde-auth-nextjs
+
+install framer motion with: pnpm install framer-motion
---
+++ src/components/Phone.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { cn } from '@/lib/utils'
+import { HTMLAttributes } from 'react'
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string
+  dark?: boolean
+}
+
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        'relative pointer-events-none z-50 overflow-hidden',
+        className
+      )}
+      {...props}>
+      <img
+        src={
+          dark
+            ? '/phone-template-dark-edges.png'
+            : '/phone-template-white-edges.png'
+        }
+        className='pointer-events-none z-50 select-none'
+        alt='phone image'
+        // width={300}
+        // height={300}
+      />
+
+      <div className='absolute -z-10 inset-0'>
+        <img
+          className='object-cover min-w-full min-h-full'
+          src={imgSrc}
+          alt='overlaying phone image'
+          // width={300}
+          // height={300}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Phone
